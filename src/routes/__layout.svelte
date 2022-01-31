d2bab688e0e9535c27d32025339e861bd29b8cae
Alexander Nortung <alex_nortung@live.dk>
2022-01-31 17:15:01 +0100

    vite error will now appear.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,28 @@
+
+<script context="module">
+  import { lang } from "$lib/stores/lang";
+  import { t, locales, locale, loadTranslations } from "$lib/translations";
+
+  export const load = async ({ url }) => {
+    const { pathname } = url;
+
+    const defaultLocale = get(lang); // get from cookie, user session, ...
+
+    const initLocale = locale.get() || defaultLocale; // set default if no locale already set
+
+    await loadTranslations(initLocale, pathname);
+
+    return {};
+  };
+</script>
+
+<script lang="ts">
+  import "../app.css";
+
+  import { get } from "svelte/store";
+</script>
+
+<main>
+  Test
+  <slot />
+</main>
